5b4274bba09369eabe79f4669d236a340adaf3df
giulioproia93@gmail.com <abKS^hedPsQ+5t/>
2020-10-22 16:48:41 +0200

    v1.1

--- background.js
+++ background.js
@@ -8,7 +8,7 @@
 
   var c;
   var c2; 
-  
+  alert("ciao")
     const interval = setInterval(function() {
         c= document.getElementsByClassName("tw-button tw-button--success tw-interactive");
        if(c.length === 0 ){
@@ -20,12 +20,12 @@
        }
 
        c2= document.getElementsByClassName("ytp-ad-text ytp-ad-skip-button-text");
-       if(c.length === 0 ){
+       if(c2.length === 0 ){
            console.log("non c'è");
        }
        else{
             console.log("trovato");
-            c[0].click();
+            c2[0].click();
 
        }
      }, 5000);
@@ -34,3 +34,17 @@
     
     
 
+    //  "page_action": {
+    //   "default_icon": {
+    //     "16": "images/kekimg16x16.png",
+    //     "32": "images/kekimg32x32.png",
+    //     "48": "images/kekimg48x48.png",
+    //     "128": "images/kekimg128x128.png"
+    //   }
+    // },
+  
+    // "icons": {
+    //   "16": "images/kekimg16x16.png",
+    //   "32": "images/kekimg32x32.png",
+    //   "48": "images/kekimg48x48.png",
+    //   "128": "images/kekimg128x128.png"
